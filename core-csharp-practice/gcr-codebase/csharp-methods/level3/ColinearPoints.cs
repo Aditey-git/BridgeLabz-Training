5c1089d2c404cd6ffe0023a02e23cd3e1c25a8af
Aditey-git <raiaditey@gmail.com>
2025-12-26 02:56:51 +0530

    [Aditey Rai] Added theremaining level 3 of Methods Assignment

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level3/ColinearPoints.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Methods.level3
+{
+    internal class ColinearPoints
+    {
+        public static void CollinearPoints() {
+            //Getting input from user
+            Console.Write("Enter x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter x3: ");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter y3: ");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            //Calling Methods
+            bool slope = CollinearUsingSlopeCheck(x1, y1, x2, y2, x3, y3);
+            bool area = CollinearUsingAreaCheck(x1, y1, x2, y2, x3, y3);
+
+            Console.WriteLine("\nUsing Slope Method: " +(slope ? "Points are Collinear" : "Points are NOT Collinear"));
+            Console.WriteLine("Using Area Method: " + (area ? "Points are Collinear" : "Points are NOT Collinear"));
+        }
+
+
+        //Method to check collinearity using slope formula
+        public static bool CollinearUsingSlopeCheck(double x1, double y1, double x2, double y2, double x3, double y3){
+
+            double ABslope = (y2 - y1)/(x2 - x1);
+            double BCslope = (y3 - y2)/(x3 - x2);
+            double ACslope = (y3 - y1)/(x3 - x1);
+
+            return (ABslope == BCslope && BCslope == ACslope);
+
+        }
+
+        //Method to check collinearity using area of triangle
+        public static bool CollinearUsingAreaCheck(double x1, double y1, double x2, double y2, double x3, double y3){
+
+            double area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+            return area == 0;
+
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level3/OTP.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Methods.level3
+{
+    internal class OTP
+    {
+        public static void OTPfn() {               
+                int[] arrayOfOTP = new int[10];
+
+                // Generating OTP
+                for(int i = 0; i < arrayOfOTP.Length; i++){
+                    arrayOfOTP[i] = GenerateOTP();
+                    Console.WriteLine("Generated OTP " + (i + 1) + ": " + arrayOfOTP[i]);
+                }
+
+                bool isOTPUnique = OTPsUniqueCheck(arrayOfOTP);
+
+                if(isOTPUnique){
+                    Console.WriteLine("Generated OTPs are unique");
+                }
+                else{
+                    Console.WriteLine("OTPs are not unique");
+                }
+        }
+
+        //Method to generate a OTP
+        public static int GenerateOTP(){
+            Random random = new Random();
+            return random.Next(100000, 1000000);
+        }
+
+        //Method to check uniqueness
+        public static bool OTPsUniqueCheck(int[] otps){
+
+            for(int i = 0; i < otps.Length; i++){
+                for(int j = i + 1; j < otps.Length; j++){
+                    if(otps[i] == otps[j]){
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        }
+    }
+}

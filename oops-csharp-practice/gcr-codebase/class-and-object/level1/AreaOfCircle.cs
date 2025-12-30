14d63eafe5735cf13516cb5cb0d60ab91555a229
Aditey-git <raiaditey@gmail.com>
2025-12-30 16:20:33 +0530

    [Aditey Rai] Added level 1 of class and object

---
+++ oops-csharp-practice/gcr-codebase/class-and-object/level1/AreaOfCircle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Oops.level1
+{
+    internal class AreaOfCircle
+    {
+        // Attribute
+        public double radius;
+        public static void AreaOfCircleImplement()
+        {
+            //Object creation
+            AreaOfCircle cir = new AreaOfCircle();
+
+            //User Input
+            Console.Write("Enter radius of circle: ");
+            cir.radius = Convert.ToDouble(Console.ReadLine());
+
+            //Display area and circumference
+            cir.AreaCalculate();
+            cir.CircumferenceCalculate();
+        }
+
+        // Method to calculate and display area
+        public void AreaCalculate()
+        {
+            double areaOfCircle = Math.PI * radius * radius;
+            Console.WriteLine("Area of the Circle: " + areaOfCircle);
+        }
+
+        // Method to calculate and display circumference
+        public void CircumferenceCalculate(){
+            double circumferenceOfCircle = 2 * Math.PI * radius;
+            Console.WriteLine("Circumference of the Circle: " + circumferenceOfCircle);
+        }
+
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/class-and-object/level1/EmplayeeDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Oops.level1
+{
+    internal class EmployeeDetails
+    {
+        public string name;
+        public int id;
+        public double salary;
+
+
+        //Class for doing object creation and method call
+        public static void DetailsOfEmployee()
+        {
+            //Object creation
+            EmployeeDetails employee = new EmployeeDetails();
+
+            employee.name = "Aditey";
+            employee.id = 1;
+            employee.salary = 50000;
+            employee.DisplayEmployeeDetails();
+        }
+             
+        //Method to display Employee  
+        public void DisplayEmployeeDetails(){
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Salary: " + salary);
+            }
+        }
+}

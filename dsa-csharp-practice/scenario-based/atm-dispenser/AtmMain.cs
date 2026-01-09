5ccafdfb40688f11da235bb23965b4b82f1717d0
Aditey-git <raiaditey@gmail.com>
2026-01-09 16:31:24 +0530

    [Aditey Rai] Added Atm dispenser code

---
+++ dsa-csharp-practice/scenario-based/atm-dispenser/AtmMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining2.DSA.Scenario_based.atm_dispenser
+{
+    internal class AtmMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Atm");
+            AtmUtility atm = new AtmUtility();
+            Console.WriteLine("Scenario A -> Given ₹1, ₹2, ₹5, ₹10, ₹20, ₹50, ₹100, ₹200, ₹500 notes, find optimal.");
+            atm.ScenarioA();
+            Console.WriteLine("Scenario B -> Remove ₹500 temporarily and update strategy.");
+            atm.ScenarioB();
+            Console.WriteLine("Scenario C -> Display fallback combo if exact change isn’t possible.");
+            atm.ScenarioC();
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/atm-dispenser/AtmUtility.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining2.DSA.Scenario_based.atm_dispenser
+{
+    internal class AtmUtility
+    {
+        public void ScenarioA()
+        {
+            int[] notesArr = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+            int[] notesCount = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Console.Write("Enter the amount to debit from your account --> ");
+            int debitAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int index = notesArr.Length - 1;
+            while (debitAmount > 0)
+            {
+                if (debitAmount >= notesArr[index])
+                {
+                    debitAmount -= notesArr[index];
+                    notesCount[index]++;
+                }
+                else
+                {
+                    index--;
+                }
+            }
+            for (int i = 0; i < notesArr.Length; i++)
+            {
+                if (notesCount[i] > 0)
+                {
+                    Console.WriteLine($"{notesArr[i]} --> {notesCount[i]}");
+                }
+            }
+        }
+
+        public void ScenarioB()
+        {
+            
+            int[] notesArr = { 1, 2, 5, 10, 20, 50, 100, 200 };
+            int[] notesCountArr = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            Console.Write("Enter the amount to debit from your account --> ");
+            int debitAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int index = notesArr.Length - 1;
+            while (debitAmount > 0)
+            {
+                if (debitAmount >= notesArr[index])
+                {
+                    debitAmount -= notesArr[index];
+                    notesCountArr[index]++;
+                }
+                else
+                {
+                    index--;
+                }
+            }
+            for (int i = 0; i < notesArr.Length; i++)
+            {
+                if (notesCountArr[i] > 0)
+                {
+                    Console.WriteLine($"{notesArr[i]} --> {notesCountArr[i]}");
+                }
+            }
+        }
+
+        public void ScenarioC()
+        {
+            int[] notesArr = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+            int[] notesCountArr = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int[] notesDispensedArr = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Console.Write("Enter the amount to debit from your account --> ");
+            int debitAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int index = notesArr.Length - 1;
+            while (debitAmount > 0 && index >= 0)
+            {
+                if (debitAmount >= notesArr[index])
+                {
+                    if (notesCountArr[index] > 0)
+                    {
+                        debitAmount -= notesArr[index];
+                        notesCountArr[index]--;
+                        notesDispensedArr[index]++;
+                    }
+                    if (notesCountArr[index] == 0)
+                    {
+                        index--;
+                    }
+                }
+                else
+                {
+                    index--;
+                }
+            }
+            if (debitAmount > 0)
+            {
+                Console.WriteLine("Failed to give the desired amount due to shortage of notes.");
+            }
+            else
+            {
+                for (int i = 0; i < notesArr.Length; i++)
+                {
+                    if (notesCountArr[i] == 0)
+                    {
+                        Console.WriteLine($"{notesArr[i]} --> {notesDispensedArr[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
+

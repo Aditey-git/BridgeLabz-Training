481b1219e77f9c365bf52fff503d32f77dc033db
Aditey-git <raiaditey@gmail.com>
2026-01-14 15:24:31 +0530

    [Aditey Rai] Added the ability to add new contact in the Address book array

---
+++ oops-csharp-practice/scenario-based/AddressBookSystem/AddressBookUtility.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining2.Oops.AddressBookSystem
+{
+    internal class AddressBookUtility
+    {
+        private Contact[] contactsArr;
+        private int index = 0;
+
+        public AddressBookUtility()
+        {
+            contactsArr = new Contact[20];
+        }
+
+        public void AddAContact()
+        {
+
+            if (index >= contactsArr.Length)
+            {
+                Console.WriteLine("Address Book is full!");
+                return;
+            }
+
+            Console.Write("Enter the first name of the contact:");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the last name of the contact:");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter the address:");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter the city:");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter the state:");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter the zipcode:");
+            string zipcode = Console.ReadLine();
+
+            Console.Write("Enter the phone number:");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter the email:");
+            string email = Console.ReadLine();  
+
+
+            Contact contact = new Contact(name, lastName, address, city, state, zipcode, phoneNumber, email);
+            contactsArr[index] = contact;
+            index++;
+
+
+            Console.WriteLine("Contact added successfully");
+        }
+
+    }
+}
--- oops-csharp-practice/scenario-based/AddressBookSystem/Contact.cs
+++ oops-csharp-practice/scenario-based/AddressBookSystem/Contact.cs
@@ -10,76 +10,28 @@ namespace BridgeLabzTraining2.Oops.AddressBookSystem
     //This is the encapsulated class which give layout for Address book
     internal class Contact
     {
-        private string firstName;
-        private string lastName;
-        private string address;
-        private string city;
-        private string state;   
-        private string zipCode;
-        private string phoneNumber;
-        private string email;
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Address{ get; private set; }
+        public string City{ get; private set; }
+        public string State   { get; private set; }
+        public string ZipCode { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
 
-        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode,string phoneNumber,string email)
+        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string email)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zipCode = zipCode;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.ZipCode = zipCode;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
         }
         
 
-        //Getters and Setters Method 
-        public string FirstName { 
-            get { return firstName; }
-            set { firstName = value; } 
-        }
         
-        public string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
-
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
-        
-        public string City
-        {
-            get { return city; }
-            set { city = value; }
-        }
-
-
-        public string State
-        {
-            get{ return state; }
-            set { state = value; }
-        }
-
-        public string ZipCode
-        {
-            get { return zipCode; }
-            set { zipCode = value; }
-        }
-
-
-        public string PhoneNumber
-        {
-            get { return phoneNumber; }
-            set { phoneNumber = value; }
-        }
-
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
     }
 }

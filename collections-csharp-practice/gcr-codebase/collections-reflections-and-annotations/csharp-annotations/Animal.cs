a4c4fb1eb1c6536159c9943ef612f1e3cc349ede
Aditey-git <raiaditey@gmail.com>
2026-01-28 02:50:35 +0530

    [Aditey Rai] Added the questions of reflections and annotations

---
+++ collections-csharp-practice/gcr-codebase/collections-reflections-and-annotations/csharp-annotations/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTrainingCollectionsAAndFurther3.Annotations.cs
+{
+    internal class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+
+    class AnimalKingdom
+    {
+        public static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.MakeSound();   
+        }
+    }
+}
